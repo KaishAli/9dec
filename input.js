90654ec5b6efabd4a9b8cc6cfa71a11035b798ca
KaishAli <91947877+KaishAli@users.noreply.github.com>
2021-12-12 16:45:56 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,12 @@
+function kaish(a) {
+    a = document.getElementById("num1").value;
+    return function (b) {
+        b = document.getElementById("num2").value;
+        return function (c) {
+            c = document.getElementById("num3").value;
+            alert("The result is = " + a * b * c);
+            
+        }
+    }
+}
+kaish(a)(b)(c);
